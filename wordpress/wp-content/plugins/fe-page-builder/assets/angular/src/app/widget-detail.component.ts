f184f382a57aa4cc48d899941ec4f8b5960e3c01
jimmyman2010 <tranduyminhman@gmail.com>
2017-04-25 21:13:34 +0700

    fepb

--- wordpress/wp-content/plugins/fe-page-builder/assets/angular/src/app/widget-detail.component.ts
+++ wordpress/wp-content/plugins/fe-page-builder/assets/angular/src/app/widget-detail.component.ts
@@ -3,18 +3,7 @@ import { Widget } from './widget';
 
 @Component({
   selector: 'widget-detail',
-  template: `
-    <div *ngIf="hero">
-      <h2>{{widget.name}} details!</h2>
-      <div>
-        <label>id: </label>{{widget.id}}
-      </div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="widget.name" placeholder="name"/>
-      </div>
-    </div>
-  `
+  templateUrl: './../templates/widget-detail.component.html'
 })
 export class WidgetDetailComponent {
   @Input() widget: Widget;
